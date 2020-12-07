6779c8d00bfa0bf9d378ac80a6b6e56e1ea1d9f7
Jackson <jackson.barnes162@gmail.com>
2020-12-07 14:48:22 +0000

    Day 1 complete, Day 2 next

---
+++ Day 1 of 100.playground/Contents.swift
@@ -0,0 +1,42 @@
+//variable
+var str = "Hello, Playground."
+str = "Goodbye, playground."
+
+var age = 38
+var population = 8_000_000
+
+//multi line strings
+var str1 = """
+This goes
+over multiple
+line
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 95
+
+//String Interpolation
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+//constants
+
+let taylor = "Swift"
+
+//Type annotations
+let str4: String
+let year: Int
+let month: Int
+let day: Int
+let leapYear: Bool
+
+
+
