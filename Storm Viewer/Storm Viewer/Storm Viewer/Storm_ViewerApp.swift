b0c3b9df2c59119759287d95fdd9682d4d2478dd
Jackson <jackson.barnes162@gmail.com>
2020-12-24 15:08:45 +0000

    Initial init of Storm Viewer, day 16
    NOT DOING THIS, CHANGING FROM UIKIT TO SWIFTUI

---
+++ Storm Viewer/Storm Viewer/Storm Viewer/Storm_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Storm_ViewerApp.swift
+//  Storm Viewer
+//
+//  Created by Jackson Barnes on 24/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Storm_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
