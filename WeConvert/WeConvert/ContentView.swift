df121f3d361132c9dde7324bb72075857b75974c
Jackson <jackson.barnes162@gmail.com>
2020-12-26 16:08:58 +0000

    WeConvert initial commit

---
+++ WeConvert/WeConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeConvert
+//
+//  Created by Jackson Barnes on 26/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeConvert/WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Jackson Barnes on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
