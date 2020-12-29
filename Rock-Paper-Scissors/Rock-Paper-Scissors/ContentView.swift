227d82eb364a23224fe8e1df3faf33c35fc51435
Jackson <jackson.barnes162@gmail.com>
2020-12-29 13:42:30 +0000

    Initial Commit - RPS - Day 25

---
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Jackson Barnes on 29/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
