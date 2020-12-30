fb49a93a893dc4404e035642f9c6aa19360c8b25
Jackson <jackson.barnes162@gmail.com>
2020-12-30 13:47:05 +0000

    Initial Commit for BetterRest Day 26

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jackson Barnes on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
