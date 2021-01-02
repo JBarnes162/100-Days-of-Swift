1ad38e8c8307a9896b3039c41908c04b4bdf2ea1
Jackson <jackson.barnes162@gmail.com>
2021-01-02 15:03:04 +0000

    Initial Commit for Word Scramble Day 29

---
+++ WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Jackson Barnes on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
