3eb4d5f5e080fb0b10757a80bf9da9b621ecd43a
Jackson <jackson.barnes162@gmail.com>
2021-01-03 19:03:15 +0000

    Guess the flag Animations added
    Checked a forum due to not knowing really how to do it, much more needed to be done compared to what I expected, read this code and understand fully.

--- Guess The Flag/Guess The Flag/ContentView.swift
+++ Guess The Flag/Guess The Flag/ContentView.swift
@@ -12,8 +12,8 @@ extension Image {
         self
             .renderingMode(.original)
             .clipShape(Capsule())
-            .overlay(Capsule().stroke(Color.black, lineWidth: 3))
-            .shadow(color: .black, radius: 2)
+            .overlay(Capsule().stroke(Color.black, lineWidth: 2))
+            .shadow(color: .black, radius: 1)
    }
 }
 
@@ -27,10 +27,15 @@ struct ContentView: View {
     @State private var userScore = 0
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var animationAmount = 0.0
+    @State private var userTapped = 0
+    @State private var opacity = 1.0
+    @State private var offset = CGFloat.zero
+    @State private var disabled = false
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 30) {
                 VStack {
@@ -42,13 +47,34 @@ struct ContentView: View {
                         .fontWeight(.black)
                 }
                 
-                ForEach(0..<3) { number in
+                ForEach(0 ..< 3) { number in
                     Button(action: {
-                        self.flagTapped(number)
-                    }) {
+                        if number == self.correctAnswer {
+                            self.flagTapped(number)
+                            self.userTapped = number
+                            withAnimation(.easeInOut(duration: 2)) {
+                                self.animationAmount += 360
+                                self.opacity -= 0.75
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2)
+                                {self.askQuestions()}
+                        } else {
+                            self.flagTapped(number)
+                            self.userTapped = number
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                self.offset = 200
+                            }
+                        }
+                        self.disabled = true
+                    })  {
                         Image(self.countries[number])
                             .flagImage()
                     }
+                    .rotation3DEffect(.degrees(self.animationAmount), axis: (x: 0, y: number == self.userTapped ? 1 : 0, z : 0))
+                    .offset(x: number != self.correctAnswer ? self.offset : .zero, y: .zero)
+                    .clipped()
+                    .opacity(number != self.userTapped ? self.opacity: 1.0)
+                    .disabled(self.disabled)
                 }
                 Text("Your Current score is: \(userScore)")
                     .foregroundColor(.white)
@@ -76,6 +102,9 @@ struct ContentView: View {
     }
     
     func askQuestions() {
+        self.disabled = false
+        self.opacity = 1
+        self.offset = .zero
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
