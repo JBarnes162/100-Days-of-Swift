7d47a2050b4dc08487fc77c76aa26c379a674cc0
Jackson <jackson.barnes162@gmail.com>
2021-01-06 18:27:07 +0000

    Added new view for questions, and passing data from one view to another
    Maybe change the way I create new views so there isnt a back button, a whole new screen rather than a cover

--- Let Me Edutain You/Let Me Edutain You/ContentView.swift
+++ Let Me Edutain You/Let Me Edutain You/ContentView.swift
@@ -50,15 +50,17 @@ struct ContentView: View {
     @State private var animalIcon = 17
     
     @State private var gameState = false                            // True for Playing - False for Settings
-    @State private var multplicationTable = 1                       // Which multiplication table do they want questions for
-    @State private var numberOfQuestion = 0                         // How many questions do they want to answer
-    @State private var multiplyBy = Int.random(in: 0...11)          // To decide what to multiply the number with
+    @State private var multplicationTable = 0                       // Which multiplication table do they want questions for
+    @State private var numberOfQuestions = 1                        // How many questions do they want to answer
     @State private var studentName = "Default Name"
+    @State private var question = 0
     
-    let numberOfQuestions = ["5", "10", "20", "All"]
+    
+    let questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     
     
     var body: some View {
+        NavigationView {
             Group {
                 ZStack {
                     LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue]), startPoint: .topLeading
@@ -75,7 +77,7 @@ struct ContentView: View {
                                 .font(.title2)
                         }
                     
-                        Text("     Let Me Edutain You!")
+                        Text("     Let me Edutain You!")
                         .frame(width: 160, height: 75, alignment: .center)
                         .font(.title)
                         .titleStyle()
@@ -84,8 +86,8 @@ struct ContentView: View {
                     Form{
                         Section(header: Text("What times table do you want to do?").foregroundColor(.white)){
                             Stepper(value: $multplicationTable, in:
-                                        1...12, step: 1) {
-                                Text("\(multplicationTable) times table")
+                                        0...11, step: 1) {
+                                Text("\(questions[multplicationTable]) times table")
                             }
                         }   .textCase(nil)
                             .font(.title3)
@@ -94,29 +96,66 @@ struct ContentView: View {
                             
                       
                         Section(header: Text("How many questions do you want?").foregroundColor(.white)) {
-                            Picker("How many questions:", selection: $numberOfQuestion) {
-                                ForEach(0 ..< numberOfQuestions.count) {
-                                    Text("\(self.numberOfQuestions[$0])")
-                                }
+                            Stepper(value: $numberOfQuestions, in:
+                                        1...25, step: 1) {
+                                Text("\(numberOfQuestions) questions")
                             }
-                            .pickerStyle(SegmentedPickerStyle())
                         }   .textCase(nil)
                             .font(.title3)
                             .foregroundColor(.black)
                         
                     }
-                    
                     VStack{
-                        Button("Start!") {
-                            // Do nothing
-                            }
-                        .titleStyle()
-                        .font(.title)
-                    }
+                        NavigationLink(destination: QuestionView(timesTable: questions[multplicationTable], questionAmount: numberOfQuestions)) {
+                            Text("Start!")
+                            .titleStyle()
+                            .font(.title)
+                        }
+                    }.navigationBarTitle("Quit", displayMode: .inline)
+                }
             }
-        }
+        }.navigationBarTitle("Let me Edutain you!")
+            .navigationBarHidden(true)
     }
 }
+    
+    
+    struct QuestionView: View {
+        
+        @State private var multiplyBy = Int.random(in: 0...11)          // To decide what to multiply the number with
+        
+        let timesTable: Int
+        let questionAmount: Int
+        
+        var correctAnswer: Int {
+            let answer = timesTable * multiplyBy
+            return answer
+        }
+        
+        var body: some View {
+            Text("Hello \(timesTable) times table, \(questionAmount) questions")
+            Text("\(correctAnswer), \(multiplyBy)")
+        }
+        
+    }
+    
+    struct HomeView: View {
+            @Binding var buttonClick: Bool
+
+            var body: some View {
+                VStack {
+                    Button(action: {
+                        withAnimation {
+                            self.buttonClick.toggle()
+                        }
+                    }) {
+                        Text("Settings")
+                    }.buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
