aca050cfa23c84a7dd5b0647af83c21be459628b
Jackson <jackson.barnes162@gmail.com>
2021-01-21 12:28:02 +0000

    Day 43 Complete, drawing basics

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jackson Barnes on 21/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
