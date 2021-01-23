e8a8dcf3a06bfabab7180e131da9c9c1ff578fa5
Jackson <jackson.barnes162@gmail.com>
2021-01-23 10:40:46 +0000

    Day 44 Complete

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,35 +7,12 @@
 
 import SwiftUI
 
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount: CGFloat = 0
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-    var arc = self
-    arc.insetAmount += amount
-    return arc
-    }
-}
 
 
 struct ContentView: View {
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
+        Text("Hello")
     }
 }
 
@@ -93,3 +70,116 @@ struct ContentView_Previews: PreviewProvider {
 //Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
 //    .stroke(Color.blue, lineWidth: 10)
 //    .frame(width: 300, height: 300)
+
+//struct Arc: InsettableShape {
+//    var startAngle: Angle
+//    var endAngle: Angle
+//    var clockwise: Bool
+//    var insetAmount: CGFloat = 0
+//
+//    func path(in rect: CGRect) -> Path {
+//        let rotationAdjustment = Angle.degrees(90)
+//        let modifiedStart = startAngle - rotationAdjustment
+//        let modifiedEnd = endAngle - rotationAdjustment
+//
+//        var path = Path()
+//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+//
+//        return path
+//    }
+//
+//    func inset(by amount: CGFloat) -> some InsettableShape {
+//    var arc = self
+//    arc.insetAmount += amount
+//    return arc
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+//            .strokeBorder(Color.blue, lineWidth: 40)
+//    }
+//}
+
+//Drawing a 16 elipses and positioning them around a centre point with sliders to alter the appeareance
+
+//struct Flower: Shape {
+//    var petalOffset: Double = -20
+//    var petalWidth: Double = 100
+//
+//    func path(in rect: CGRect) -> Path{
+//        var path = Path()
+//
+//        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8){
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//
+//            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            path.addPath(rotatedPetal)
+//        }
+//        return path
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//    @State private var petalOffset = -20.0
+//    @State private var petalWidth = 100.0
+//    var body: some View {
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                .fill(Color.red, style: FillStyle(eoFill: true))
+//
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40)
+//                .padding([.horizontal, .bottom])
+//
+//            Text("Width")
+//            Slider(value: $petalWidth, in: 0...100)
+//                .padding(.horizontal)
+//        }
+//    }
+//}
+
+// Rainbow circle with slider, using DrawingGroup() to maintain performance
+//struct ColorCyclingCircle: View {
+//    var amount = 0.0
+//    var steps = 100
+//
+//    var body: some View {
+//        ZStack {
+//            ForEach(0..<steps) { value in
+//                Circle()
+//                    .inset(by: CGFloat(value))
+//                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+//            }
+//        }
+//        .drawingGroup()
+//    }
+//
+//    func color(for value: Int, brightness: Double) -> Color {
+//        var targetHue = Double(value) / Double(self.steps) + self.amount
+//
+//        if targetHue > 1 {
+//            targetHue -= 1
+//        }
+//
+//        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var colorCycle = 0.0
+//    var body: some View {
+//        VStack {
+//            ColorCyclingCircle(amount: self.colorCycle)
+//                .frame(width: 300, height: 300)
+//
+//            Slider(value: $colorCycle)
+//        }
+//    }
+//}
